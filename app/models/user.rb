233cc4c0b7ffec6b295067844c65830084124f75
Johnny Girault <esbo@hotmail.fr>
2020-05-09 17:14:30 +0200

    add destroy model + controller + routes + schema datetimes

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,21 @@ class User < ApplicationRecord
       where(friend_id: id, status: 'pending')
   end
 
+  # instead of deleting, indicate the user requested a delete & timestamp it
+  def soft_delete
+    update_attribute(:deleted_at, Time.current)
+  end
+
+  # ensure user account is active
+  def active_for_authentication?
+    super && !deleted_at
+  end
+
+  # provide a custom message for a deleted account
+  def inactive_message
+    !deleted_at ? super : :deleted_account
+  end
+
   include PgSearch
 
   pg_search_scope :search,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_162754) do
+ActiveRecord::Schema.define(version: 2020_05_09_150554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_162754) do
     t.string "phone_number"
     t.string "avatar"
     t.string "address"
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
