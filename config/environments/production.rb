40163e9cc5f68acb8f4d7b188efb83d85cf6b6f1
Johnny Girault <esbo@hotmail.fr>
2020-05-31 18:15:38 +0200

    config.active_storage.variable_content_types

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :amazon
 
+  config.active_storage.variable_content_types = %w(
+    image/png
+    image/jpg
+    image/jpeg
+  )
+
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
